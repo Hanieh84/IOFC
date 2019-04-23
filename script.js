fd5e28b9ce95c5d88a5c99c4d3d6c7c1b9f2a4f8
Hanieh <hanieh@Haniehs-MBP.lan>
2019-04-23 22:19:32 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,151 @@
+$('#DropDownLeft').change(function () {
+    var selectedText = $(this).find("option:selected").text();
+
+    switch (selectedText) {
+        case "Hindi":
+            converterFileLeft("Files/TLFTHindi.txt");
+            $("#headerLeft").text("चखोई हुई परी कथाएँ");
+            break;
+        case "Arabic":
+            converterFileLeft("Files/TLFTArabic.txt");
+            $("#headerLeft").text("القصص الخيالية المفقودة");
+            break;
+        case "German":
+            converterFileLeft("Files/TLFTGerman.txt");
+            $("#headerLeft").text("Die verlorenen Märchen");
+            break;
+        case "Farsi":
+            converterFileLeft("Files/TLFTFarsi.txt");
+            $("#headerLeft").text("داستان های افسانه ای فراموش شده از جن و پری");
+
+            break;
+        case "Japanese":
+            converterFileLeft("Files/TLFTJapanese.txt");
+            $("#headerLeft").text("ロストおとぎ話");
+            break;
+        case "Pashto":
+            converterFileLeft("Files/TLFTPashto.txt");
+            $("#headerLeft").text("ورک شوي خاپیری قصي");
+            break;
+        case "English":
+            converterFileLeft("Files/TLFTEnglish.txt");
+            $("#headerLeft").text("The Lost Fairy Tales");
+            break;
+        case "Chinese":
+            converterFileLeft("Files/TLFTChinese.txt");
+            $("#headerLeft").text("失落的童话故事");
+            break;
+        case "Russian":
+            converterFileLeft("Files/TLFTRussian.txt");
+            $("#headerLeft").text("Потерянные сказки");
+            break;
+        case "Somali":
+            converterFileLeft("Files/TLFTSomali.txt");
+            $("#headerLeft").text("Tallooyinka Caqli Celinta Toosan");
+            break;
+        case "Spanish":
+            converterFileLeft("Files/TLFTSpanish.txt");
+            $("#headerLeft").text("Los cuentos de hadas perdidos");
+            break;
+        case "Svenska":
+            converterFileLeft("Files/TLFTsvenskversion.txt");
+            $("#headerLeft").text("De glömda sagorna");
+            break;
+        case "French":
+            converterFileLeft("Files/TLFTFrench.txt");
+            $("#headerLeft").text("Les Contes de Fées Perdus");
+            break;
+
+
+        default:
+            console.log("no choises");
+    }
+});
+
+$('#DropDownRight').change(function () {
+    var selectedText = $(this).find("option:selected").text();
+
+    switch (selectedText) {
+        case "Hindi":
+            converterFileRight("Files/TLFTHindi.txt");
+            $("#headerRight").text("चखोई हुई परी कथाएँ");
+            break;
+        case "Arabic":
+            converterFileRight("Files/TLFTArabic.txt");
+            $("#headerRight").text("القصص الخيالية المفقودة");
+            break;
+        case "German":
+            converterFileRight("Files/TLFTGerman.txt");
+            $("#headerRight").text("Die verlorenen Märchen");
+            break;
+        case "Farsi":
+            converterFileRight("Files/TLFTFarsi.txt");
+            $("#headerRight").text("داستان های افسانه ای فراموش شده از جن و پری");
+            break;
+        case "Japanese":
+            converterFileRight("Files/TLFTJapanese.txt");
+            $("#headerRight").text("ロストおとぎ話");
+            break;
+        case "Pashto":
+            converterFileRight("Files/TLFTPashto.txt");
+            $("#headerRight").text("ورک شوي خاپیری قصي");
+            break;
+        case "English":
+            converterFileRight("Files/text.txt");
+            $("#headerRight").text("The Lost Fairy Tales");
+            break;
+        case "Chinese":
+            converterFileRight("Files/TLFTChinese.txt");
+            $("#headerRight").text("失落的童话故事");
+            break;
+        case "Russian":
+            cconverterFileRight("Files/TLFTRussian.txt");
+            $("#headerRight").text("Потерянные сказки");
+            break;
+        case "Somali":
+            converterFileRight("Files/TLFTSomali.txt");
+            $("#headerRight").text("Tallooyinka Caqli Celinta Toosan");
+            break;
+        case "Spanish":
+            converterFileRight("Files/TLFTSpanish.txt");
+            $("#headerRight").text("Los cuentos de hadas perdidos");
+            break;
+        case "Svenska":
+            converterFileRight("Files/TLFTsvenskversion.txt");
+            $("#headerRight").text("De glömda sagorna");
+            break;
+        case "French":
+            converterFileRight("Files/TLFTFrench.txt");
+            $("#headerRight").text("Les Contes de Fées Perdus");
+            break;
+        default:
+            console.log("no choises");
+    }
+
+});
+
+function converterFileLeft(input) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", input, false);
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
+                $("#resultTextLeft").text(rawFile.responseText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+function converterFileRight(input) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", input, false);
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
+                $("#resultTextRight").text(rawFile.responseText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
